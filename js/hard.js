397d85f22b36df9525760967459c93690e7ee145
nikit <03nikit72@gmail.com>
2018-11-24 01:34:59 +0600

    Это мой первый коммит

---
+++ js/hard.js
@@ -0,0 +1,12 @@
+var num = 33721;
+
+var num1 = Math.floor(num / 10000);
+var num2 = Math.floor(num%10000/1000);
+var num3 = Math.floor(num%10000%1000/100);
+var num4 = Math.floor(num%10000%1000%100/10);
+var num5 = Math.floor(num%10000%1000%100%10);
+var summ = num1+num2+num3+num4+num5;
+var mul  = num1*num2*num3*num4*num5;
+
+console.log(summ);
+console.log(Math.pow(mul,3));
---
+++ js/script.js
@@ -0,0 +1,12 @@
+var Money = + prompt('Какой у вас бюджет ?');
+var StoreName = prompt('Название вашего магазина');
+var MainList = {
+    money      : Money,
+    storeName  : StoreName,
+    shopGroups : [],
+    open : true,
+}
+MainList.shopGroups[0] = prompt('Какой тип товаров будем продовать ?','Car');
+MainList.shopGroups[1] = prompt('Какой тип товаров будем продовать ?','House');
+MainList.shopGroups[3] = prompt('Какой тип товаров будем продовать ?','Phones');
+console.log('Ваш бюджет на 1 день ' + MainList.money/30);
